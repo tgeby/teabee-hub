f3cee05e31339bcd1ce108a6f88ee19d6c608ca0
Thomas Eby <tgeby@icloud.com>
2025-09-02 14:28:12 -0400

    Added auth0 and associated buttons.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
+        authorizationParams = {{
+          redirect_uri: import.meta.env.VITE_AUTH_CALLBACK_URL
+        }}
+      >
+        <App/>
+      </Auth0Provider>
+  </StrictMode>
 )
